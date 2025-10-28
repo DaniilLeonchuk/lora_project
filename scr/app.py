2d8d8394df7dcec9aca4e8549c20177d272ec044
DaniilLeonchuk <157125623+DaniilLeonchuk@users.noreply.github.com>
2025-10-29 01:42:29 +0300

    Добавил полурабочий flask

--- scr/app.py
+++ scr/app.py
@@ -3,11 +3,20 @@ import psycopg2
 import os
 from datetime import datetime
 
-DB_CONFIG = [
+app = Flask(__name__)
+
+DB_CONFIG = {
     'host': 'localhost',
     'database': 'lora_monitoring',
     'user': 'lora_user',
     'password': 'lora_password',
     'port': 5432
-]
+}
 
+@app.route('/')
+def rout():
+    return 'Hello'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
