4caf48b59be0832d96d21dd4df9ccd59eff2f93c
TimurPodshivalov <157318954+TimurPodshivalov@users.noreply.github.com>
2025-12-08 23:01:49 +0300

    Обвновил функцию get_updates, чтобы она возвращала только уникальные id

--- scr/userid.py
+++ scr/userid.py
@@ -3,8 +3,13 @@ import requests
 def get_updates(token):
     url = f'https://api.telegram.org/bot{token}/getUpdates'
     response = requests.get(url)
-    return response.json()
-
+    response=response.json()
+    seen_chat_ids = set()
+    for update in response['result']:
+        chat_id = update['message']['chat']['id']
+        if chat_id not in seen_chat_ids:
+            seen_chat_ids.add(chat_id)
+    return seen_chat_ids
 
 def send_telegram_message(token, chat_id, message):
     url = f"https://api.telegram.org/bot{token}/sendMessage"
